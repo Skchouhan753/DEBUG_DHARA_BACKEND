c8786467457b78fb8a73aca1f5a98102d418952a
Skchouhan753 <s.k.chouhan753@gmail.com>
2024-04-04 12:31:39 +0530

    update 11.0

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
  
 require("dotenv").config();
 
-// const connection = mongoose.connect(process.env.MONGO_URI);
+const connection = mongoose.connect(process.env.MONGO_URI);
 
-const connection = mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+// const connection = mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
 
 module.exports = {
     connection
